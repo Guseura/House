0fd4650036a4cb879232e5c79ac677dbaf2b8b53
Guseura <gooseura@gmail.com>
2022-03-07 12:08:32 +0200

    added AppIcon and Firebase, created Authorization screen

--- House/AppDelegate.swift
+++ House/AppDelegate.swift
@@ -1,11 +1,5 @@
-//
-//  AppDelegate.swift
-//  House
-//
-//  Created by Yurij on 02.03.2022.
-//
-
 import UIKit
+import Firebase
 import CoreData
 
 @main
@@ -14,6 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        FirebaseApp.configure()
         State.shared.setLanguage(to: Language.Code.init(Locale.current.regionCode ?? "en"))
         return true
     }
---
+++ House/Screens/Authorization/Controllers/AuthorizationViewController.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+class AuthorizationViewController: BaseViewController {
+
+    // MARK: - @IBOutlets
+    
+    // Views
+    // Labels
+    // Buttons
+    // Image Views
+    // ...
+    
+    // MARK: - Variables
+    
+    // MARK: - Awake functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        configureUI()
+        setupGestures()
+    }
+    
+    // MARK: - Custom functions
+    
+    override func configureUI() {
+        
+    }
+    
+    override func setupGestures() {
+        
+    }
+    
+    // MARK: - Gesture actions
+    
+    // MARK: - @IBActions
+    
+}
